95efe78470cf7d94f70493683f6f304cc7a0b471
ClayData <claytonortiz96@gmail.com>
2020-05-13 13:25:42 -0500

    added navbar and routing

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import MenuButton from './MenuButton';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <MenuButton />
+          <Typography variant="h6" className={classes.title}>
+            Book-Self
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
