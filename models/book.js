54daa350ab1dfdcc3aafa3b14416a63e17cd4a19
ClayData <claytonortiz96@gmail.com>
2020-05-14 15:11:48 -0500

    added book controller

--- models/book.js
+++ models/book.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: { type: String, required: true},
     author: { type: String, required: true},
+    image: { type: String, required: true},
     description: String,
     date: {type: Date, default: Date.now}
 })
