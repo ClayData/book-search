bd903033b8f7c6553542d6d2df53a7d383e79f4d
ClayData <claytonortiz96@gmail.com>
2020-05-16 20:22:22 -0500

    changed postbuild script again

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Start the API server
 app.listen(PORT, function() {
