dad2ad9fdd0239bd446e41ee53eff3cc2664a3b8
ClayData <claytonortiz96@gmail.com>
2020-06-02 13:59:56 -0500

    eslint on backend files

--- models/book.js
+++ models/book.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: { type: String, required: true},
-    user: { type: String, required: true},
-    authors: [ String ],
-    image: { type: String },
-    view: { type: String },
-    description: String,
-    date: {type: Date, default: Date.now}
-})
+  title: { type: String, required: true },
+  user: { type: String, required: true },
+  authors: [String],
+  image: { type: String },
+  view: { type: String },
+  description: String,
+  date: { type: Date, default: Date.now }
+});
 
 const Book = mongoose.model("Book", bookSchema);
 
-module.exports = Book
+module.exports = Book;
--- server.js
+++ server.js
@@ -14,7 +14,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
 // Add routes, both API and view
 app.use(routes);
 
@@ -22,6 +21,6 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://claymo:098*()poi@ds017726.mlab.com:17726/heroku_3hczf2nx");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
